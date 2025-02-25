df91a472a5d61916fa99092d54062e153a3e09b6
troubitsine <troubitsine@gmail.com>
2025-02-25 01:43:43 +0000

    installed analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import { TempoDevtools } from "tempo-devtools";
 TempoDevtools.init();
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename={basename}>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>,
 );
