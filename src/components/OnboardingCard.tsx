5f332af1ea488819655a11f8da9e843082a8e203
troubitsine <troubitsine@gmail.com>
2025-03-13 01:54:43 +0000

    some visual edits to onboarding card

--- src/components/OnboardingCard.tsx
+++ src/components/OnboardingCard.tsx
@@ -33,17 +33,19 @@ const OnboardingCard = ({ className = "" }: OnboardingCardProps) => {
       className={`w-full lg:w-[65vw] min-w-[300px] max-w-[1800px] mx-auto mb-6 ${className}`}
     >
       <div
-        className={`px-4 sm:px-6 py-3 sm:py-4 bg-gradient-to-br from-neutral-400/80 to-neutral-600/80 rounded-xl backdrop-blur-md transition-all duration-300 ${isCollapsed ? "h-14" : ""}`}
+        className={`px-4 py-3 sm:py-4 bg-neutral-500/60
+               before:absolute before:inset-0 before:bg-gradient-to-br before:from-white/10 before:to-transparent before:rounded-xl before:pointer-events-none
+               backdrop-blur-md rounded-xl transition-all duration-300 ${isCollapsed ? "h-14.5" : ""}`}
       >
         <div className="flex justify-between items-center">
           <h2 className="text-lg sm:text-xl font-semibold text-white/90">
             Focus Reel
           </h2>
           <Button
-            variant="ghost"
             size="sm"
+            variant="secondary"
             onClick={toggleCollapsed}
-            className="text-white/80 hover:text-white hover:bg-white/10 rounded-full px-4"
+            className="bg-white/75 hover:bg-white/65 before:absolute before:inset-0 before:bg-gradient-to-b before:from-transparent before:to-black/20 before:rounded-full text-black/75 backdrop-blur-md flex items-center gap-2 rounded-full inner-stroke-white-20-sm px-[11px]  py-[6px]"
           >
             {isCollapsed ? "About" : "Hide"}
           </Button>
@@ -99,10 +101,10 @@ const OnboardingStep = ({
 }: OnboardingStepProps) => {
   return (
     <div className="flex flex-col items-center">
-      <div className="w-7 h-7 bg-white/75 rounded-full backdrop-blur-sm flex items-center justify-center mb-2">
+      <div className="w-7 h-7 bg-white/70 before:absolute before:inset-0 before:bg-gradient-to-b before:from-transparent before:to-black/20 before:rounded-full text-black/75 backdrop-blur-md flex items-center gap-2 rounded-full inner-stroke-white-20-sm flex items-center justify-center mb-2 ">
         <span className="text-black/75 text-xs font-semibold">{number}</span>
       </div>
-      <div className="bg-neutral-800/50 p-4 rounded-xl border border-white/10 mb-2 w-full max-w-[164px] h-[146px] flex items-center justify-center">
+      <div className="bg-neutral-800/50 rounded-xl border border-white/10 mb-2 w-full max-w-[164px] flex h-28 py-[4] py-[4] p-2 justify-center items-center">
         <img
           src={imageSrc}
           alt={`Step ${number}`}
